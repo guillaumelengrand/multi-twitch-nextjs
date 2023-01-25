7228facf75a41c1ddfd27250610b9bd2ae2d00ea
Guillaume Lengrand <lengra_g@epita.fr>
2023-01-25 09:32:53 +0100

    Tailwind: update config to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
+    content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {
             height: {
                 'screen-96': '96vh',
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [],
 };
